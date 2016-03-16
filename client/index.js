d446556603872d0679967b5c0ce9315c5d94ca7d
Rj Bernaldo <rj.bernaldo@gmail.com>
2016-03-16 14:46:25 +0800

    Initial dist subtree commit

--- client/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM, { render } from 'react-dom';
-import ReactPongApp from './components/ReactPongApp.react';
-
-render(<ReactPongApp />, document.getElementById('content'));
